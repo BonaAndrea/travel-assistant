d8c8b4bbf1706726513965205ded44ba4efc9fdf
Andrea Bona <andrea.bona1996@gmail.com>
2026-09-16 15:54:15 +0200

    chore: prepare separate Railway deployment

---
+++ frontend/docker-entrypoint.d/40-api-base.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -eu
+
+api_base="${API_BASE_URL:-http://localhost:4000/api}"
+
+# API_BASE_URL è una variabile Railway del solo servizio frontend.
+# In locale il Compose monta direttamente la cartella e non esegue questo script.
+find /usr/share/nginx/html -type f -name '*.js' -print0 \
+  | xargs -0 -r sed -i "s|http://localhost:4000/api|${api_base}|g"
